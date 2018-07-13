8a288bd56d95f32e3b46198b1fc5d6db68e56add
Mark Gelfius <mgelfius@purdue.edu>
2018-07-13 14:42:02 -0400

    Can calculate angles in degrees or radians Added comma seperation to large numbers Fixed rounding issues with angle calculation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 import math
+from decimal import Decimal
 
 class Application(tkinter.Frame):
     def __init__(self, master=None):
@@ -12,6 +13,7 @@ class Application(tkinter.Frame):
         self.create_widgets()
         self.calcFrame = tkinter.Frame()
         self.calcFrame.pack()
+        self.build_exp()
 
     def clearFrame(self, frame):
         for widget in frame.winfo_children():
@@ -54,41 +56,51 @@ class Application(tkinter.Frame):
     def build_ang(self):
         self.clearFrame(self.calcFrame)
         self.v = tkinter.IntVar()
-        L1 = tkinter.Label(self.calcFrame, text="Angle in radians")
+        self.r = tkinter.IntVar()
+        self.degRadio = tkinter.Radiobutton(self.calcFrame, text="Degrees", value=1, width=10, variable=self.r)
+        self.radRadio = tkinter.Radiobutton(self.calcFrame, text="Radians", value=2, width=10, variable=self.r)
+        L1 = tkinter.Label(self.calcFrame, text="Angle")
         self.angNum = tkinter.Entry(self.calcFrame, width="5")
         self.sinRadio = tkinter.Radiobutton(self.calcFrame, text="Sin", value=1, width=10, variable=self.v)
         self.cosRadio = tkinter.Radiobutton(self.calcFrame, text="Cos", value=2, width=10, variable=self.v)
         self.tanRadio = tkinter.Radiobutton(self.calcFrame, text="Tan", value=3, width=10, variable=self.v)
         self.v.set(1)
+        self.r.set(1)
         calcAngle = tkinter.Button(self.calcFrame, text="Calculate", command=self.calculateAng)
         self.answer = tkinter.Label(self.calcFrame)
         L1.grid(row = 1)
         self.angNum.grid(row=1, column=1)
-        self.sinRadio.grid(row=2, column=0)
-        self.cosRadio.grid(row=2, column=1)
-        self.tanRadio.grid(row=2, column=2)
-        calcAngle.grid(row=3)
-        self.answer.grid(row=4)
+        self.degRadio.grid(row=2, column=0)
+        self.radRadio.grid(row=2, column=1)
+        self.sinRadio.grid(row=3, column=0)
+        self.cosRadio.grid(row=3, column=1)
+        self.tanRadio.grid(row=3, column=2)
+        calcAngle.grid(row=4)
+        self.answer.grid(row=5)
 
     def calculateExpMethod(self):
-        self.product["text"] = "Answer: " + str((int(self.baseNum.get())**int(self.expNum.get())))
+        self.product["text"] = "Answer: " + str("{:,}".format((int(self.baseNum.get())**int(self.expNum.get()))))
 
     def calculateFib(self):
         n = int(self.fibNum.get())
         answer = self.fib(n)
-        self.final["text"] = "Answer: " + str(answer)
+        self.final["text"] = "Answer: " + str("{:,}".format(answer))
 
     def calculateAng(self):
+        degOrRad = " radians"
         passMath = self.readMath(self.angNum.get())
+        if(self.r.get() == 1):
+            passMath = math.radians(passMath)
+            degOrRad = " degrees"
         if self.v.get() == 1:
-            final = math.sin(passMath)
-            self.answer["text"] = "Sine of " + self.angNum.get() + ": " + str(final)
+            final = Decimal(math.sin(passMath)).quantize(Decimal('1e-4'))
+            self.answer["text"] = "Sine of " + self.angNum.get() + degOrRad + ": " + str(final)
         if self.v.get() == 2:
-            final = math.cos(passMath)
-            self.answer["text"] = "Cosine of " + self.angNum.get() + ": " + str(final)
+            final = Decimal(math.cos(passMath)).quantize(Decimal('1e-4'))
+            self.answer["text"] = "Cosine of " + self.angNum.get() + degOrRad + ": " + str(final)
         if self.v.get() == 3:
-            final = math.tan(passMath)
-            self.answer["text"] = "Tangent of " + self.angNum.get() + ": " + str(final)
+            final = Decimal(math.tan(passMath)).quantize(Decimal('1e-4'))
+            self.answer["text"] = "Tangent of " + self.angNum.get() + degOrRad + ": " + str(final)
         
 
     def fib(self, n):
@@ -102,7 +114,7 @@ class Application(tkinter.Frame):
             return self.fib(n - 2) + self.fib(n - 1)
 
     def readMath(self, passMath):
-        return eval(passMath.lower().replace('pi', 'math.pi'))
+        return  eval(passMath.lower().replace('pi', 'math.pi'))
 
 
 root = tkinter.Tk()
